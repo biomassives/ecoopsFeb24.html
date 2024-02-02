dc5e78aa11c502feb248ef0019f9d33290b775f6
G. Willson <greg@biomassiv.es>
2024-02-02 08:58:44 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <MapComponent />
+    <FileUploaderComponent />
+    <DataFormComponent />
+    <UploadToNFTStorage />
+  </div>
+</template>
+
+<script>
+import MapComponent from './components/MapComponent.vue';
+import FileUploaderComponent from './components/FileUploaderComponent.vue';
+import DataFormComponent from './components/DataFormComponent.vue';
+import UploadToNFTStorage from './components/UploadToNFTStorage.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      MapComponent,
+      FileUploaderComponent,
+      DataFormComponent,
+      UploadToNFTStorage
+    },
+  }
+
+
+</script>
+
